dbf91fb73c28698e0c9fc35a9db50d553c69f81d
nadzeya <vitserresvit@gmail.com>
2023-08-08 15:42:39 +0300

    homework6

---
+++ homework6/index.sql
@@ -0,0 +1,43 @@
+Вывести ко-во поставщиков не из UK и не из China
+
+SELECT COUNT(*) 
+FROM Suppliers 
+WHERE Country NOT IN ('UK', 'China');
+
+
+Вывести среднюю/MAX/MIN стоимости и ко-во товаров из категорий 3 и 5
+
+SELECT 
+COUNT(*) AS total_products,
+AVG(Price) AS avg_price,
+MAX(Price) AS max_price,
+MIN(Price) AS min_price
+FROM Products
+where
+ProductID IN (3, 5)
+
+Вывести общую сумму проданных товаров
+
+SELECT
+SUM(Products.Price * OrderDetails.Quantity) AS order_cost
+FROM OrderDetails
+join Products ON OrderDetails.ProductID=Products.ProductID
+
+Вывести ко-во стран, которые поставляют напитки
+
+SELECT COUNT(Suppliers.Country) AS count_country
+FROM Suppliers
+join Products on Suppliers.SupplierID=Products.SupplierID
+where
+Products.CategoryID = 1;
+
+Вывести сумму, на которую было отправлено товаров клиентам в USA
+
+
+SELECT SUM(Products.Price * OrderDetails.Quantity) AS order_cost
+FROM Customers
+join Orders on Customers.CustomerID=Orders.CustomerID
+join OrderDetails on Orders.OrderID=OrderDetails.OrderID
+join Products on OrderDetails.ProductID=Products.ProductID
+where
+Customers.Country = "USA"
