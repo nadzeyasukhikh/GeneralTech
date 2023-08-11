4e1f9f7be5ba7a6f359b1b4db940cdc1c25f7c50
nadzeya <vitserresvit@gmail.com>
2023-08-11 14:41:48 +0300

    new

--- homework6/index.sql
+++ homework6/index.sql
@@ -25,11 +25,15 @@ join Products ON OrderDetails.ProductID=Products.ProductID
 
 Вывести ко-во стран, которые поставляют напитки
 
-SELECT COUNT(Suppliers.Country) AS count_country
-FROM Suppliers
-join Products on Suppliers.SupplierID=Products.SupplierID
-where
-Products.CategoryID = 1;
+SELECT 
+   COUNT(DISTINCT suppliers.Country) AS count_countries
+FROM products
+
+JOIN suppliers ON products.SupplierID=suppliers.SupplierID
+JOIN categories ON products.CategoryID=categories.CategoryID
+
+WHERE
+   categories.CategoryName='Beverages';
 
 Вывести сумму, на которую было отправлено товаров клиентам в USA
 
